8b382f48551228b93a70a2c922d78b07cdbb08e4
Syahria <127468222+Syahria@users.noreply.github.com>
2024-05-28 19:52:44 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,39 +1,23 @@
 <?php
-// Koneksi ke database (sesuaikan dengan konfigurasi database Anda)
-$host = 'localhost'; // Host database
-$username = 'root'; // Username database
-$password = ''; // Password database
-$database = 'menara_cafeha'; // Nama database
 
-// Membuat koneksi
+// Informasi koneksi database
+$host = "localhost"; // Host database (biasanya "localhost")
+$username = "root"; // Nama pengguna database
+$password = ""; // Kata sandi database
+$database = "menara_cafeha"; // Nama database
+
+// Membuat koneksi ke database
 $conn = new mysqli($host, $username, $password, $database);
 
 // Memeriksa koneksi
 if ($conn->connect_error) {
-    die("Koneksi ke database gagal: " . $conn->connect_error);
+    die("Koneksi Gagal: " . $conn->connect_error);
 }
 
-// Memeriksa apakah form telah disubmit
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Memeriksa apakah key 'username' ada dalam $_POST
-    if(isset($_POST['username'])){
-        // Mengambil data dari form
-        $username = $_POST['username'];
-    } else {
-        // Jika key 'username' tidak ditemukan, atur $username menjadi nilai default atau lakukan tindakan lainnya
-        $username = "Nama Pengguna Tidak Tersedia";
-    }
-
-    // Mengambil data dari form
-    $ulasan = $_POST['ulasan'];
-
-    // Menyiapkan statement SQL untuk menyimpan ulasan ke dalam tabel
-    $sql = "INSERT INTO reviews (username, ulasan) VALUES ('$username', '$ulasan')";
-
-    // Menjalankan query
-    if ($conn->query($sql) === TRUE) {
-        echo "Ulasan berhasil disimpan.";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+// Fungsi untuk membersihkan input data dari potensi serangan SQL Injection
+function clean_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 }
